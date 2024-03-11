dd5a7c51b43cfdbe7ee8b4c564f25b2b4fd8fadf
Anthony Wright <alwright626@gmail.com>
2024-03-10 21:45:11 -0400

    first commit

---
+++ src/app/test-logic/test-logic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-logic',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-logic.component.html',
+  styleUrl: './test-logic.component.css'
+})
+export class TestLogicComponent {
+
+}
